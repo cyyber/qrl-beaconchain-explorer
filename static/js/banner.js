7d7368799f127ca06671cf1079a5eff29c4f24eb
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-03-21 18:26:07 +0400

    Review files part 10

--- static/js/banner.js
+++ static/js/banner.js
@@ -12,21 +12,6 @@ bannerSearchInput.addEventListener("blur", function () {
   bannerContainer.classList.remove("searching")
 })
 
-function getCookie(cname) {
-  var name = cname + "="
-  var ca = document.cookie.split(";")
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i]
-    while (c.charAt(0) == " ") {
-      c = c.substring(1)
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length)
-    }
-  }
-  return ""
-}
-
 function updateBanner() {
   fetch("/latestState")
     .then(function (res) {
